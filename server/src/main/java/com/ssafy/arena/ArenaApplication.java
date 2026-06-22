908bdae53b5e6f6e41d7432f543e4ac7001440a0
MINYONG PARK <parkminyong22@gmail.com>
2026-06-22 11:22:01 +0900

    fix : MyBatis 매퍼 스캔 범위 제한

--- server/src/main/java/com/ssafy/arena/ArenaApplication.java
+++ server/src/main/java/com/ssafy/arena/ArenaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
 
-@MapperScan("com.ssafy.arena")
+@MapperScan("com.ssafy.arena.mapper")
 @SpringBootApplication
 public class ArenaApplication {
 
